9e0e9cbed0e449df9be8221429fc888d8b9968e1
2214839296@qq.com <a921204a>
2018-10-13 18:40:07 +0800

    fix bug

--- structs/structs.go
+++ structs/structs.go
@@ -3,27 +3,29 @@ package structs
 import "time"
 
 type Index struct {
-	Id       string       `xorm:"not null pk VARCHAR(40)"`
-	Name string    `xorm:"not null VARCHAR(10)"`
-	Chapter string    `xorm:"not null VARCHAR(10)"`
-	Total int `xorm:"not null int"`
-	Update time.Time `xorm:"TIMESTAMP"`
+	Id      string    `xorm:"not null pk VARCHAR(40)"`
+	Name    string    `xorm:"not null VARCHAR(100)"`
+	Chapter string    `xorm:"not null VARCHAR(100)"`
+	Total   int       `xorm:"not null int"`
+	Update  time.Time `xorm:"TIMESTAMP"`
+	Index   int       `xorm:"null int"`
+	Url     string    `xorm:"null VARCHAR(100)"`
 	Created time.Time `xorm:"TIMESTAMP created"`
-	Flag bool
+	Flag    bool
 }
 
 type Chapter struct {
-	Id string `xorm:"not null pk VARCHAR(40)"`
-	Pid string `xorm:"not null VARCHAR(40)"`
-	Name string `xorm:"not null VARCHAR(50)"`
-	Path string `xorm:"text"`
-	Num int `xorm:"not null int"`
+	Id      string    `xorm:"not null pk VARCHAR(40)"`
+	Pid     string    `xorm:"not null VARCHAR(40)"`
+	Name    string    `xorm:"not null VARCHAR(150)"`
+	Path    string    `xorm:"text"`
+	Num     int       `xorm:"not null int"`
 	Created time.Time `xorm:"TIMESTAMP created"`
 }
 
 type UrlData struct {
-	Type string `json:"type"`
-	File string `json:"file"`
-	Label string `json:"label"`
+	Type    string `json:"type"`
+	File    string `json:"file"`
+	Label   string `json:"label"`
 	Default string `json:"default"`
 }
--- utils/datasource.go
+++ utils/datasource.go
@@ -7,21 +7,21 @@ import (
 func GetIndex(page int) []structs.Index {
 	var list []structs.Index
 	engine := GetCon()
-	engine.OrderBy("created asc").Limit(20, page*20).Find(&list)
+	engine.OrderBy("`index` asc").Limit(20, page*20).Find(&list)
 	return list
 }
 
 func SearchByName(name string) []structs.Index {
 	var list []structs.Index
 	engine := GetCon()
-	engine.Where("name like concat('%',?,'%')", name).OrderBy("created asc").Find(&list)
+	engine.Where("name like concat('%',?,'%')", name).OrderBy("`index` asc").Find(&list)
 	return list
 }
 
 func GetByName(name string) []structs.Index {
 	var list []structs.Index
 	engine := GetCon()
-	engine.Where("name=?", name).OrderBy("created asc").Find(&list)
+	engine.Where("name=?", name).OrderBy("`index` asc").Find(&list)
 	return list
 }
 
